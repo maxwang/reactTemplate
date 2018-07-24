b2fb636b583bb6ffa27b0f9a7fc5aeafe3bba68d
Max Wang <mwang@1080agile.com>
2018-07-24 10:09:11 +1000

    using provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './components/App';
 
 const node = document.getElementById("react-app");
 
-render(<App />, node);
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    node);
